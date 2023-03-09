ab2610e841d6767fa587050c9ae3e0d4238e294e
j1nxie <rylieeeeexd@gmail.com>
2023-03-10 00:58:55 +0700

    re-organize projects into workspaces

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+use common::constants::EnvironmentVariable;
+use serenity::{
+    async_trait,
+    model::{
+        application::{
+            command::Command,
+            interaction::{Interaction, InteractionResponseType},
+        },
+        gateway::Ready,
+    },
+    prelude::*,
+};
+
+mod commands;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let commands = Command::set_global_application_commands(&ctx.http, |commands| {
+            commands.create_application_command(|command| commands::ping::register(command))
+        })
+        .await;
+
+        println!("current slash commands: {:#?}", commands);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = EnvironmentVariable::DISCORD_BOT_TOKEN.value();
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("error creating client.");
+
+    if let Err(why) = client.start().await {
+        println!("client error: {:?}", why);
+    }
 }
