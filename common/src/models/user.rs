bfcd99f2941815f576b0f825cc0974ed3b87cc79
j1nxie <rylieeeeexd@gmail.com>
2023-03-12 01:10:04 +0700

    add osu_id to User

--- common/src/models/user.rs
+++ common/src/models/user.rs
@@ -3,6 +3,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub discord_id: String,
+    pub osu_id: String,
 }
