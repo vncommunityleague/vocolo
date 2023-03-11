b782a274baea02d58fd037b80957806f0da9586b
j1nxie <rylieeeeexd@gmail.com>
2023-03-12 01:10:27 +0700

    initial impl of UserRepo + OsuRepo

--- server/src/repository/mongo.rs
+++ server/src/repository/mongo.rs
@@ -1,1 +1,26 @@
+use common::models::{osu::OsuPlayer, user::User};
+use mongodb::{Client, Collection};
 
+pub struct UserRepo {
+    col: Collection<User>,
+}
+
+impl UserRepo {
+    pub async fn init(client: &Client) -> Self {
+        let db = client.database("VCL");
+        let col: Collection<User> = db.collection("User");
+        UserRepo { col }
+    }
+}
+
+pub struct OsuRepo {
+    col: Collection<OsuPlayer>,
+}
+
+impl OsuRepo {
+    pub async fn init(client: &Client) -> Self {
+        let db = client.database("VCL");
+        let col: Collection<OsuPlayer> = db.collection("OsuUser");
+        OsuRepo { col }
+    }
+}
